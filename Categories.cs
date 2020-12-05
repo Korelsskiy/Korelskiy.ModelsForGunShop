7d61cd27ebd6ac93b2aef8afef90c7568417a7d4
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-05 23:18:19 +0300

    Добавьте файлы проекта.

---
+++ Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Korelskiy.ModelsForGunShop
+{
+    public enum Cat
+    {
+        Винтовки,
+        ПП,
+        Пулеметы
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Korelskiy.ModelsForGunShop
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+       // [RegularExpression("pattern", ErrorMessage = "error message")]
+        public string Producer { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string PhotoPath { get; set; }
+        public Cat? Category { get; set; }
+    }
+}
