c5c4c77fa02a9da416c384fd95d6eec127669c3d
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-06 08:01:56 +0300

    Создана модель пользователя для регистрации

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Korelskiy.ModelsForGunShop
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50), MinLength(2)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50), MinLength(2)]
+        [
+            RegularExpression
+            (@"^([a - z0 - 9_ -] +\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$",
+            ErrorMessage ="Введите валидный емайл(example@exam.ru)")
+        ]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50), MinLength(2)]
+        public string Password { get; set; }
+    }
+}
